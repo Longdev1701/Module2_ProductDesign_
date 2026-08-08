fe15ace51feb2d7032eb2468d25410cbce1488eb
CHĂM RỐCH THI <rochthi59@gmail.com>
2026-08-08 18:11:33 +0700

    style(auth): set panel background to pure black to match branding image background

--- fe/src/features/auth/AuthBrandingPanel.tsx
+++ fe/src/features/auth/AuthBrandingPanel.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function AuthBrandingPanel() {
   return (
-    <div className="hidden lg:flex lg:w-1/2 relative bg-[#020813] items-center justify-center p-8 overflow-hidden">
+    <div className="hidden lg:flex lg:w-1/2 relative bg-black items-center justify-center p-8 overflow-hidden">
       <img 
         src="/hero_branding.png" 
         alt="Themis LexiGuard Branding" 
-        className="w-full h-auto max-h-[85vh] object-contain object-center drop-shadow-2xl"
+        className="w-full h-auto max-h-[85vh] object-contain object-center shadow-2xl"
       />
     </div>
   );
