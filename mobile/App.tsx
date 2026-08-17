f89c8f1a5698e740ea568e6e4cc7a55c35786bd0
CHĂM RỐCH THI <rochthi59@gmail.com>
2026-08-17 22:35:50 +0700

    chore(mobile): wipe 100% of mobile frontend codebase (mobile/src/ and App.tsx)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,31 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
-import { AppHeader } from './src/components/Header';
-import { UnifiedMobileShell } from './src/components/UnifiedMobileShell';
+import { View } from 'react-native';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#00143B" />
-
-      {/* App Header */}
-      <AppHeader />
-
-      {/* Main Unified 100% Web Feature Shell */}
-      <View style={styles.body}>
-        <UnifiedMobileShell />
-      </View>
-    </SafeAreaView>
-  );
+  return <View />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00143B',
-  },
-  body: {
-    flex: 1,
-    backgroundColor: '#FAF8FF',
-  },
-});
